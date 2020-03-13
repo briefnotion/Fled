a7e5cfcd234625ea252f472e72d13520d52b27a8
Brief <5898977+briefnotion@users.noreply.github.com>
2020-03-13 18:34:32 -0500

    Added Comments and Cleaned a few things.

--- Fled/Fled.ino
+++ Fled/Fled.ino
@@ -9,7 +9,7 @@
 // *                                                      (c) 2856 - 2857 Core Dynamics
 // ***************************************************************************************
 // *
-// *  PROJECTID: gi6$b*E>*q%;    Revision: 00000000.02B
+// *  PROJECTID: gi6$b*E>*q%;    Revision: 00000000.02C
 // *  TEST CODE:                 QACODE: A565              CENSORCODE: gi6$b*E>*q%;
 // *
 // ***************************************************************************************
@@ -59,11 +59,14 @@
 // *
 // *  V 0.02b _200313
 // *    - Dirty Birdy update:
-// *        The code is a big hot mess.  Nearly completely undocumented.  Crap is 
+// *        The code is a big hot mess.  Nearly completely undocumented.  Crap is
 // *        everywhere.  Stuff is scattered and unorganized.  Varibles are huge
 // *        and nothing is unorganized.  The thing is ...
 // *    - It works.
 // *    - Next step: clean this bird up to and get it ready for a stew.
+// *    0.02c   
+// *    - Quickly added comments and started cleaning things up.
+// *    - Tested to see if it still runs as before.  Yep.  All good.
 // *
 // *  V 0.02  _200311
 // *    - Creating main working system.
@@ -80,6 +83,9 @@
 // ---------------------------------------------------------------------------------------
 
 struct light_path
+//  Create a light_path:
+//    Controls the animation of a single LED over time.  Path referes to path in time.
+//    If a light path is over written the previous animation will be lost.
 {
   unsigned long tmeSTARTTIME;
   unsigned long intDURATION;
@@ -90,10 +96,12 @@ struct light_path
   boolean booFirstRun = false;
 
   void set(unsigned long tmeCurrentTime, unsigned long tmeStartInTime, unsigned long intDuration, byte bytAnimation, CRGB crgbDestinationColor)
-  //  Create a light_path:
+  //  Setup proces to prepare an animation to start at a future time.
+  //
   //  Parameters:
   //
-  //  CurrentTime   - For the cycle or actual real time of when the event is said to be created.
+  //  CurrentTime   - For the cycle or actual real time of when the event is said to be
+  //                  created.
   //  Start In Time - The delay or scheduled wait time after the event was created.
   //  Duration      - How fast, or how long the event will last.
   //  Routine       - 0 to 255.  Determines the event routine.
@@ -114,6 +122,8 @@ struct light_path
   }
 
   boolean is_ready(unsigned long tmeCurrentTime, CRGB crgbCurrentColor)
+  //  Checks to see if the light animation is ready to be executed.
+  //  Returns true if ready.
   {
     if ((booExpired == false) && (tmeCurrentTime >= tmeSTARTTIME))
     {
@@ -131,17 +141,28 @@ struct light_path
   }
 
   CRGB execute(unsigned long tmeCurrentTime)
+  //  Returns a CRGB value of the single LED respective to what its value should
+  //    be as it is in its time path.
+
+  //  LED Animations:
+  //    0 - Clear:  Clears future animation and sets to no future animation.
+  //    1 - Fade:   Fades single LED from its initial value to its destination value from
+  //                  start of animation, and completed and end of its duration.
+  //    2 - Pulse:  Pulses the LED to desired value.  The LED will have its requested
+  //                  full value at half duration time, and will return to its origional
+  //                  value at the end of the duration.
   {
     switch (bytANIMATION)
     {
       case 0:
+        // Clear Animation Path
         {
           booExpired = false;
           booFirstRun = true;
         }
       case 1:
+        // Fade Animation Path
         {
-          //Fill Color
           if (tmeCurrentTime >= tmeSTARTTIME + intDURATION)
           {
             booExpired = true;
@@ -149,8 +170,6 @@ struct light_path
           }
           else
           {
-            //int tmeElapse = tmeCurrentTime - tmeSTARTTIME;
-
             float fltPower = (float)(tmeCurrentTime - tmeSTARTTIME) / (float)intDURATION;
 
             byte R = (fltPower * crgbCOLORDEST.r) + ((1 - fltPower) * crgbCOLORSTART.r);
@@ -160,6 +179,7 @@ struct light_path
             return CRGB(R, G, B);
           } // END CASE 1
         case 2:
+          // Pulse Animation Path
           {
             if (tmeCurrentTime >= tmeSTARTTIME + intDURATION)
             {
@@ -170,19 +190,15 @@ struct light_path
             {
               unsigned long tmeElapsed = tmeCurrentTime - tmeSTARTTIME;
               float fltPower;
-              
-              if ((tmeElapsed *2) <= (intDURATION))
+
+              if ((tmeElapsed * 2) <= (intDURATION))
               {
-                fltPower = (float)(tmeElapsed) *2 / ((float)intDURATION);
+                fltPower = (float)(tmeElapsed) * 2 / ((float)intDURATION);
               }
               else
               {
-                fltPower = 1 - (((float)((tmeElapsed *2) - intDURATION) / (float)intDURATION));
+                fltPower = 1 - (((float)((tmeElapsed * 2) - intDURATION) / (float)intDURATION));
               }
-              
-              //            byte R = (fltPower * crgbCOLORDEST.r);
-              //            byte G = (fltPower * crgbCOLORDEST.g);
-              //            byte B = (fltPower * crgbCOLORDEST.b);
 
               byte R = (fltPower * crgbCOLORDEST.r) + ((1 - fltPower) * crgbCOLORSTART.r);
               byte G = (fltPower * crgbCOLORDEST.g) + ((1 - fltPower) * crgbCOLORSTART.g);
@@ -200,6 +216,9 @@ struct light_path
 // ---------------------------------------------------------------------------------------
 
 struct timed_event
+//  Create a Timed LED Event:
+//    Controls when a group of LEDs are to begin their individual light path animation.
+//    If an event is over written the previous animation will be lost.
 {
   unsigned long tmeSTARTTIME;
   unsigned long intDURATION;
@@ -212,6 +231,8 @@ struct timed_event
   boolean booExpired = true;
 
   void set(light_path lpLightPaths[], unsigned long tmeCurrentTime, unsigned long tmeStartInTime, unsigned long intDuration, unsigned int intSpeed, byte bytAnimation, byte bytLEDAnimation, CRGB crgbColor, int intStartPos, int intEndPos)
+  //  Setup proces to prepare multiple LEDs to start at future time.
+  
   //  Create a light event:
   //  Parameters:
 
@@ -234,7 +255,6 @@ struct timed_event
     intSPEED = intSpeed;
     bytANIMATION = bytAnimation;
     bytLEDANIMATION = bytLEDAnimation;
-    bytLEDANIMATION;
     crgbCOLORDEST = crgbColor;
     intSTARTPOS = intStartPos;
     intENDPOS = intEndPos;
@@ -242,6 +262,8 @@ struct timed_event
   }
 
   boolean is_ready(unsigned long tmeCurrentTime)
+  //  Checks to see if the event animation is ready to be executed.
+  //  Returns true if ready.
   {
     if ((booExpired == false) && (tmeCurrentTime >= tmeSTARTTIME))
     {
@@ -254,14 +276,26 @@ struct timed_event
   }
 
   void execute(light_path lpLedArray[], unsigned long tmeCurrentTime)
+  //  Sets all requested light paths, start to end position, to begin their animation
+  //    at a future time.
+
+  //  Event Animations:
+  //    0 - Clear:  Clears future event and sets to no future future.
+  //    1 - Sweep:  Sets light paths for all LEDs from start to end posion.
+  //                  Speed is the time difference in miliseconds between each adjacent
+  //                  light path.  
+  //                  Duration, destination color, and LED animation will also be passed
+  //                  to its light path.
   {
     switch (bytANIMATION)
     {
       case 0:
+        // Clear Event
         {
           booExpired = false;
         }
       case 1:
+        // Sweep Event
         {
           unsigned long tmeStart;
           for (int x = intSTARTPOS; x <= intENDPOS; x++)
@@ -303,15 +337,12 @@ struct timed_event
 #define COLOR_ORDER GRB
 #define NUM_LEDS    60
 
-
 #define BRIGHTNESS        96  // Using Example Code.  Max unknown
 #define FRAMES_PER_SECOND 120 // Will not be necessary, but keeping, for now, just in case.
 
 
 #define NUM_TIMED_EVENTS  10  // Untill I can remember how pointers work, this is being
 // hardcoded.
-
-
 
 
 // ***************************************************************************************
@@ -332,9 +363,7 @@ int intX = 0;
 //    intRestTime defines the amount of time, in milliseconds, passed before another data
 //    read pass is performed and transmitted from the controller to the main system.
 
-// unsigned long tmeCurrentMillis = millis();          // 1 Second = 1000 millis
-unsigned long tmeCurrentMillis = millis();   // PROBLEMS WITH 32bit opperation
-// on 16 bit boards. Grrr.
+unsigned long tmeCurrentMillis = millis();   
 
 unsigned long tmePrevMillis = 0;
 int intRestTime = 0;             // Do not check for update until rest time is passed.
@@ -368,12 +397,7 @@ const boolean booTest = false;
 
 void setup()
 {
-
-  //tempvars
-  //CRGB tmpColor;
   // Prep and define Hardware.
-
-
 
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(hwLED, NUM_LEDS).setCorrection(TypicalLEDStrip);
   //hwLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
@@ -384,10 +408,8 @@ void setup()
   //  Serial.begin(9600);
   //}
 
-  // Startup Routines
-
-
-  // Clear all lights and wait 2 seconds
+  // Startup Routines:
+  // Clear all lights and wait 2 seconds only to see if the lights are working.
   fill_solid( &(hwLED[0]), NUM_LEDS, CRGB( 0, 0, 128) );
   FastLED.show();
   delay(100);
@@ -395,10 +417,11 @@ void setup()
   FastLED.show();
   delay(2000);
 
-
-
+  // Make sure we have the currentn time before we try any test animations.
   tmeCurrentMillis = millis();
-  // Clear the field in 1 second immediatly.
+  
+  // TEST ANIMATIONS DEFINED AT STARTUP
+
   tmeEvent[0].set(lpLED, tmeCurrentMillis, 1000, 50, 10, 1, 1, CRGB(50, 0, 0), 1, 59);
   tmeEvent[1].set(lpLED, tmeCurrentMillis, 4000, 250, 20, 1, 2, CRGB(50, 50, 0), 1, 59);
   tmeEvent[2].set(lpLED, tmeCurrentMillis, 8000, 500, 30, 1, 2, CRGB(50, 50, 0), 1, 59);
@@ -416,32 +439,32 @@ void setup()
   //tmeEvent[9].set(lpLED, tmeCurrentMillis, 12000, 300, 300, 1, 1, CRGB(0, 0, 0), 1, 30);
   //tmeEvent[1].set(lpLED, tmeCurrentMillis, 11000, 500, 1, CRGB(50,50,00), 33, 35);
   tmeEvent[9].set(lpLED, tmeCurrentMillis, 22000, 250, 1, 1, 1, CRGB(0, 0, 0), 1, 59);
-  
-
 
 }
 
-
-// ***   Scratpad
-// leds[i].setRGB( 255, 68, 221);
-// FastLED.show(); //Write it out.
+// ---------------------------------------------------------------------------------------
 
 void loop()
+//  Main Loop:
+//    Events and Light Path animations should only be called when their time is up.
 {
-  // put your main code here, to run repeatedly:
+
+  //  Only update the hardware when changes have been detected.
   boolean booUpdate = false;
 
+  //  Get current time.  This will be our timeframe to work in.
   tmeCurrentMillis = millis();
-  // Begin of Delay less Loop
+  
+  // --- Begin of Delayless Loop ---
   if (tmeCurrentMillis - tmePrevMillis >= intRestTime)
   {
     tmePrevMillis = tmeCurrentMillis;
 
-    // Grabbing Data ---
+    // --- Grabbing Data From Hardware inputs ---
 
-    // Process Data ---
+    // --- Process Data From Hardware Inputs ---
 
-    // Check and Execute Timed Events ---
+    // --- Check and Execute Timed Events That Are Ready ---
     for (int x = 0; x < NUM_TIMED_EVENTS; x++)
     {
       if (tmeEvent[x].is_ready(tmeCurrentMillis) == true)
@@ -450,31 +473,23 @@ void loop()
       }
     }
 
-    // Check and Execute Display Events ---
-
-
+    // --- Check and Execute Light Path Events That Are Ready ---
     for (int x = 0; x < NUM_LEDS; x++)
     {
       if (lpLED[x].is_ready(tmeCurrentMillis, hwLED[x]) == true)
       {
-        //CRGB rgbTemp = lpLED[x].getcolor();
-
-        //hwLED[x].setRGB(10, 10, 10);
-
-        //hwLED[x].setRGB(lpLED[x].execute().r,lpLED[x].execute().g,lpLED[x].execute().b);
-        //hwLED[x].setRGB(x*3,x*5,x*10);
-        //hwLED[x].setRGB(10, 10, 10);
         hwLED[x] = (CRGB)lpLED[x].execute(tmeCurrentMillis);
 
-
+        //  If we made it to this part of the code then we need to
+        //    tell the LED hardware that it has a change to commit.
         booUpdate = true;
       }
     }
 
-    // Execute Post Processes ---
-
+    // --- Execute LED Hardware Changes If Anything Was Updated ---
     if (booUpdate == true)
     {
+      // TEMPORARY CODE: FLASH LED 1 RED WHILE SOMETHING GOT UPDATED.   
       hwLED[0] = CRGB(25, 0, 0);
       FastLED.show();
       //delay(2);
@@ -482,12 +497,11 @@ void loop()
       FastLED.show();
     }
 
-
     // Debug Routines ---
     // Slow delay if in testing mode.
     if (booTest == true)
     {
       intRestTime = 250;
     }
-  }   // End Delay less Loop
+  }   // End Delayless Loop
 }
